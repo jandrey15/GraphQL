cb6863a380f47a4f26362049e7833bf444306380
john andrey <andrey_201015@hotmail.com>
2018-11-19 22:08:28 -0500

    Agregando shema y resolvers

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const { ApolloServer } = require('apollo-server-express')
+// const bodyParser = require('body-parser')
 
 const app = express()
+
+// construye un schema, usando GraphQL
+const typeDefs = require('./schema')
+
+// provee resolvers para nuestro esquema
+const resolvers = {
+  Query: {
+    // cursos: () => []
+    cursos: () => {
+      return [{
+        id: 1,
+        titulo: 'Curso de react.js',
+        descripcion: 'Aprendiendo React.js'
+      }, {
+        id: 2,
+        titulo: 'Curso de GraphQL.js',
+        descripcion: 'Aprendiendo GraphQL.js'
+      }]
+    }
+  },
+  Curso: {
+    profesor: () => {
+      return {
+        nombre: 'John Serrano'
+      }
+    }
+  }
+}
+
+// inicializar apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+
+// aplicar middleware de apollo en express
+server.applyMiddleware({
+  app
+})
 
 const PORT = 5678
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}`)
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
 })
--- schema.js
+++ schema.js
@@ -2,13 +2,43 @@ const { gql } = require('apollo-server-express')
 
 // creamos los esquemas
 const schema = gql`
+  """Definimos la entidad Curso"""
   type Curso {
     id: ID!
     titulo: String!
+    """Esta es la descripción del curso"""
+    description: String!
+    profesor: Profesor
+    rating: Float @deprecated(reason: "No creemos más en los puntajes")
+    comentarios: [Comentario]
+  }
+
+  """Definimos la entidad Profesor"""
+  type Profesor {
+    id: ID!
+    nombre: String!
+    nacionalidad: String!
+    genero: Genero
+    cursos: [Curso]
+  }
+
+  enum Genero {
+    MASCULINO
+    FEMENINO
+  }
+
+  """Definimos la entidad Comentario"""
+  type Comentario {
+    id: ID!
+    nombre: String!
+    cuerpo: String!
   }
 
   type Query {
     cursos: [Curso]
+    profesores: [Profesor]
+    curso(id: Int): Curso
+    profesor(id: Int): Profesor
   }
 `
 
